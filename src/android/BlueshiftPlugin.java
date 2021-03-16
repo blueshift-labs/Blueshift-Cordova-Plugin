429d23a1b7c017e341bbf5625e4a8845f3a0c721
Rahul Raveendran V P <rahul.raveendran@getblueshift.com>
2021-03-16 14:38:55 +0530

    [MOBL-776] add basic methods  (#1)
    renamed plugin class to BlueshiftPlugin to avoid name conflict
    added logs for debugging
    added methods to the js file
    Added methods to Java file

---
+++ src/android/BlueshiftPlugin.java
@@ -0,0 +1,312 @@
+package com.blueshift.cordova;
+
+import android.util.Log;
+
+import com.blueshift.Blueshift;
+import com.blueshift.BlueshiftAppPreferences;
+import com.blueshift.inappmessage.InAppApiCallback;
+import com.blueshift.model.UserInfo;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class BlueshiftPlugin extends CordovaPlugin {
+    private static final String TAG = "BlueshiftPlugin";
+
+    private Blueshift getBlueshiftInstance() {
+        return Blueshift.getInstance(cordova.getContext());
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // IN APP MESSAGES
+        if (action.equals("registerForInAppMessages")) return registerForInAppMessages(args);
+        if (action.equals("unregisterForInAppMessages")) return unregisterForInAppMessages();
+        if (action.equals("fetchInAppMessages")) return fetchInAppMessages(callbackContext);
+        if (action.equals("displayInAppMessages")) return displayInAppMessages();
+
+        // EVENTS
+        if (action.equals("trackCustomEvent")) return trackCustomEvent(args);
+        if (action.equals("identify")) return identify(args);
+
+        // USER INFO
+        if (action.equals("setUserInfoEmailID")) return setUserInfoEmailID(args);
+        if (action.equals("setUserInfoCustomerID")) return setUserInfoCustomerID(args);
+        if (action.equals("setUserInfoFirstName")) return setUserInfoFirstName(args);
+        if (action.equals("setUserInfoLastName")) return setUserInfoLastName(args);
+        if (action.equals("setUserInfoExtras")) return setUserInfoExtras(args);
+        if (action.equals("removeUserInfo")) return removeUserInfo();
+
+        // LIVE CONTENT
+        if (action.equals("getLiveContentByEmail"))
+            return getLiveContentByEmail(args, callbackContext);
+        if (action.equals("getLiveContentByCustomerID"))
+            return getLiveContentByCustomerID(args, callbackContext);
+        if (action.equals("getLiveContentByDeviceID"))
+            return getLiveContentByDeviceID(args, callbackContext);
+
+        // OPT IN/OUT
+        if (action.equals("enableTracking")) return enableTracking(args);
+        if (action.equals("enablePush")) return enablePush(args);
+        if (action.equals("enableInApp")) return enableInApp(args);
+
+        return false;
+    }
+
+    private boolean registerForInAppMessages(JSONArray args) throws JSONException {
+        String screenName = args.getString(0);
+        // blueshift.registerForInAppMessages(cordova.getActivity(), screenName);
+        Log.d(TAG, "registerForInAppMessages: { \"screenName\" : \"" + screenName + "\"}");
+
+        getBlueshiftInstance().registerForInAppMessages(cordova.getActivity());
+
+        return true;
+    }
+
+    private boolean unregisterForInAppMessages() {
+        Log.d(TAG, "unregisterForInAppMessages: ");
+
+        getBlueshiftInstance().unregisterForInAppMessages(cordova.getActivity());
+
+        return true;
+    }
+
+    private boolean fetchInAppMessages(CallbackContext callbackContext) {
+        Log.d(TAG, "fetchInAppMessages: ");
+
+        getBlueshiftInstance().fetchInAppMessages(new InAppApiCallback() {
+            @Override
+            public void onSuccess() {
+                callbackContext.success();
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                callbackContext.error(s);
+            }
+        });
+
+        return true;
+    }
+
+    private boolean displayInAppMessages() {
+        Log.d(TAG, "displayInAppMessages: ");
+
+        getBlueshiftInstance().displayInAppMessages();
+
+        return true;
+    }
+
+    private boolean trackCustomEvent(JSONArray args) throws JSONException {
+        String eventName = args.getString(0);
+        JSONObject extras = getJSONObject(args, 1);
+        boolean canBatch = args.getBoolean(2);
+
+        Log.d(TAG, "trackCustomEvent: {\"event\":\"" + eventName + "\", \"extras\": " + extras + ", \"canBatch\": " + canBatch + "}");
+
+        getBlueshiftInstance().trackEvent(eventName, getMap(extras), canBatch);
+
+        return true;
+    }
+
+    private boolean identify(JSONArray args) throws JSONException {
+        String eventName = "identify";
+        JSONObject extras = getJSONObject(args, 0);
+        boolean canBatch = args.getBoolean(1);
+
+        Log.d(TAG, "identify: {\"event\":\"" + eventName + "\", \"extras\": " + extras + ", \"canBatch\": " + canBatch + "}");
+
+        getBlueshiftInstance().trackEvent(eventName, getMap(extras), canBatch);
+
+        return true;
+    }
+
+    private boolean setUserInfoEmailID(JSONArray args) throws JSONException {
+        String email = args.getString(0);
+
+        Log.d(TAG, "setUserInfoEmailID: " + email);
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            userInfo.setEmail(email);
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean setUserInfoCustomerID(JSONArray args) throws JSONException {
+        String customerID = args.getString(0);
+
+        Log.d(TAG, "setUserInfoCustomerID: " + customerID);
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            userInfo.setRetailerCustomerId(customerID);
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean setUserInfoFirstName(JSONArray args) throws JSONException {
+        String firstName = args.getString(0);
+
+        Log.d(TAG, "setUserInfoFirstName: " + firstName);
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            userInfo.setFirstname(firstName);
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean setUserInfoLastName(JSONArray args) throws JSONException {
+        String lastName = args.getString(0);
+
+        Log.d(TAG, "setUserInfoLastName: " + lastName);
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            userInfo.setLastname(lastName);
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean setUserInfoExtras(JSONArray args) throws JSONException {
+        JSONObject extras = getJSONObject(args, 0);
+
+        Log.d(TAG, "setUserInfoExtras: " + extras);
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            userInfo.setDetails(getMap(extras));
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean removeUserInfo() {
+        Log.d(TAG, "removeUserInfo: ");
+
+        UserInfo userInfo = UserInfo.getInstance(cordova.getContext());
+        if (userInfo != null) {
+            // TODO clear user info
+            userInfo.save(cordova.getContext());
+        }
+
+        return true;
+    }
+
+    private boolean getLiveContentByEmail(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        String slotName = args.getString(0);
+        JSONObject liveContentContext = getJSONObject(args, 1);
+
+        Log.d(TAG, "getLiveContentByEmail: {\"slot\":\"" + slotName + "\", \"live_context\":" + liveContentContext + "}");
+
+        getBlueshiftInstance().getLiveContentByEmail(slotName, getMap(liveContentContext), callbackContext::success);
+
+        return true;
+    }
+
+    private boolean getLiveContentByCustomerID(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        String slotName = args.getString(0);
+        JSONObject liveContentContext = getJSONObject(args, 1);
+
+        Log.d(TAG, "getLiveContentByCustomerID: {\"slot\":\"" + slotName + "\", \"live_context\":" + liveContentContext + "}");
+
+        getBlueshiftInstance().getLiveContentByCustomerId(slotName, getMap(liveContentContext), callbackContext::success);
+
+        return true;
+    }
+
+    private boolean getLiveContentByDeviceID(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        String slotName = args.getString(0);
+        JSONObject liveContentContext = getJSONObject(args, 1);
+
+        Log.d(TAG, "getLiveContentByDeviceID: {\"slot\":\"" + slotName + "\", \"live_context\":" + liveContentContext + "}");
+
+        getBlueshiftInstance().getLiveContentByDeviceId(slotName, getMap(liveContentContext), callbackContext::success);
+
+        return true;
+    }
+
+    private boolean enableTracking(JSONArray args) throws JSONException {
+        boolean isEnabled = args.getBoolean(0);
+        if (args.length() == 1) {
+            Log.d(TAG, "enableTracking: {\"enabled\":" + isEnabled + "}");
+
+            Blueshift.setTrackingEnabled(cordova.getContext(), isEnabled);
+        } else if (args.length() == 2) {
+            boolean wipeData = args.getBoolean(1);
+            Log.d(TAG, "enableTracking: {\"enabled\":" + isEnabled + ", \"wipeData\":" + wipeData + "}");
+
+            Blueshift.setTrackingEnabled(cordova.getContext(), isEnabled, wipeData);
+        }
+
+        return true;
+    }
+
+    private boolean enablePush(JSONArray args) throws JSONException {
+        boolean isEnabled = args.getBoolean(0);
+        Log.d(TAG, "enablePush: {\"enabled\":" + isEnabled + "}");
+        BlueshiftAppPreferences.getInstance(cordova.getContext()).setEnablePush(isEnabled);
+        BlueshiftAppPreferences.getInstance(cordova.getContext()).save(cordova.getContext());
+
+        return true;
+    }
+
+    private boolean enableInApp(JSONArray args) throws JSONException {
+        boolean isEnabled = args.getBoolean(0);
+        Log.d(TAG, "enableInApp: {\"enabled\":" + isEnabled + "}");
+        // TODO implement this
+
+        return true;
+    }
+
+    JSONObject getJSONObject(JSONArray jsonArray, int index) {
+        if (jsonArray != null && index >= 0) {
+            try {
+                return jsonArray.getJSONObject(index);
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+
+        return null;
+    }
+
+    HashMap<String, Object> getMap(JSONObject jsonObject) {
+        if (jsonObject != null) {
+            HashMap<String, Object> map = new HashMap<>();
+
+            Iterator<String> keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                try {
+                    String key = keys.next();
+                    Object val = jsonObject.get(key);
+                    map.put(key, val);
+                } catch (JSONException ignored) {
+                }
+            }
+
+            return map;
+        }
+
+        return null;
+    }
+}
--- www/Blueshift.js
+++ www/Blueshift.js
@@ -1,5 +1,180 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Blueshift', 'coolMethod', [arg0]);
+/**
+* Registers a page for showing in-app message.
+*
+* @param {String} screenName    Name of the screen.
+*/
+exports.registerForInAppMessages = function (screenName) {
+    exec(null, null, 'Blueshift', 'registerForInAppMessages', [screenName]);
+};
+
+/**
+* Unregisters a page from showing in-app message.
+*/
+exports.unregisterForInAppMessages = function () {
+    exec(null, null, 'Blueshift', 'unregisterForInAppMessages', []);
+};
+
+/**
+* Fetches in-app messages from the Blueshift API and provides them in the callbacks.
+* 
+* @param {function} success    Success callback with the in-app API response.
+* @param {function} error      Error callback with an error message.
+*/
+exports.fetchInAppMessages = function (success, error) {
+    exec(success, error, 'Blueshift', 'fetchInAppMessages', []);
+};
+
+/**
+* Display in-app message if the current page is registered for in-app messages.
+*/
+exports.displayInAppMessages = function () {
+    exec(null, null, 'Blueshift', 'displayInAppMessages', []);
+};
+
+/**
+* Send any custom event to Blueshift.
+* 
+* @param {String} eventName    Name of the custom event.
+* @param {Object} extras       Additional params (if any).
+* @param {Boolean} canBatch    Tells if this event can be batched or not.
+*/
+exports.trackCustomEvent = function (eventName, extras, canBatch) {
+    exec(null, null, 'Blueshift', 'trackCustomEvent', [eventName, extras, canBatch]);
+};
+
+/**
+* Sends an identify event with the details available.
+* 
+* @param {Object} extras   Additional params (if any)
+* @param {String} canBatch Tells if this event can be batched or not.
+*/
+exports.identify = function (extras, canBatch) {
+    exec(null, null, 'Blueshift', 'identify', [extras, canBatch]);
+};
+
+/**
+* Save email in the SDK.
+* 
+* @param {String} email email of the customer.
+*/
+exports.setUserInfoEmailID = function (email) {
+    exec(null, null, 'Blueshift', 'setUserInfoEmailID', [email]);
+};
+
+/**
+* Save customerId in the SDK.
+* 
+* @param {String} customerId customerId of the customer.
+*/
+exports.setUserInfoCustomerID = function (customerId) {
+    exec(null, null, 'Blueshift', 'setUserInfoCustomerID', [customerId]);
+};
+
+/**
+* Save firstname in the SDK.
+* 
+* @param {String} firstname firstname of the customer.
+*/
+exports.setUserInfoFirstName = function (firstname) {
+    exec(null, null, 'Blueshift', 'setUserInfoFirstName', [firstname]);
+};
+
+/**
+* Save lastname in the SDK.
+* 
+* @param {String} lastname lastname of the customer.
+*/
+exports.setUserInfoLastName = function (lastname) {
+    exec(null, null, 'Blueshift', 'setUserInfoLastName', [lastname]);
+};
+
+/**
+* Save additional user info in the SDK.
+* 
+* @param {Object} extras additional user info.
+*/
+exports.setUserInfoExtras = function (extras) {
+    exec(null, null, 'Blueshift', 'setUserInfoExtras', [extras]);
+};
+
+/**
+* Remove all the saved user info from the SDK.
+*/
+exports.removeUserInfo = function () {
+    exec(null, null, 'Blueshift', 'removeUserInfo', []);
+};
+
+/**
+* Calls Blueshift's live content API with email and given slot name and live content context.
+* 
+* @param {String} slot slot name of the live content.
+* @param {Object} lcContext live content context.
+* @param {function} success success callback.
+* @param {function} error error callback.
+*/
+exports.getLiveContentByEmail = function (slot, lcContext, success, error) {
+    exec(success, error, 'Blueshift', 'getLiveContentByEmail', [slot, lcContext]);
+};
+
+/**
+* Calls Blueshift's live content API with customer id and given slot name and live content context.
+* 
+* @param {String} slot slot name of the live content.
+* @param {Object} lcContext live content context.
+* @param {function} success success callback.
+* @param {function} error error callback.
+*/
+exports.getLiveContentByCustomerID = function (slot, lcContext, success, error) {
+    exec(success, error, 'Blueshift', 'getLiveContentByCustomerID', [slot, lcContext]);
+};
+
+/**
+* Calls Blueshift's live content API with device id and given slot name and live content context.
+* 
+* @param {String} slot slot name of the live content.
+* @param {Object} lcContext live content context.
+* @param {function} success success callback.
+* @param {function} error error callback.
+*/
+exports.getLiveContentByDeviceID = function (slot, lcContext, success, error) {
+    exec(success, error, 'Blueshift', 'getLiveContentByDeviceID', [slot, lcContext]);
+};
+
+/**
+ * Enable/disable SDK's event tracking.
+ * 
+ * @param {Boolean} enabled When true, tracking is enabled. When false, disabled.
+ */
+exports.enableTracking = function (enabled) {
+    exec(null, null, 'Blueshift', 'enableTracking', [enabled]);
+};
+
+/**
+ * Enable/disable SDK's event tracking.
+ * 
+ * @param {Boolean} enabled When true, tracking is enabled. When false, disabled.
+ * @param {Boolean} wipeData When true, events will be wiped. When false, nothing happens.
+ */
+exports.enableTracking = function (enabled, wipeData) {
+    exec(null, null, 'Blueshift', 'enableTracking', [enabled, wipeData]);
+};
+
+/**
+ * Opt-in or opt-out of push notifications sent from Blueshift.
+ * 
+ * @param {Boolean} enabled When true, opt-in else opt-out.
+ */
+exports.enablePush = function (enabled) {
+    exec(null, null, 'Blueshift', 'enablePush', [enabled]);
+};
+
+/**
+ * Opt-in or opt-out of in-app notifications sent from Blueshift.
+ * 
+ * @param {Boolean} enabled When true, opt-in else opt-out.
+ */
+exports.enableInApp = function (enabled) {
+    exec(null, null, 'Blueshift', 'enableInApp', [enabled]);
 };
